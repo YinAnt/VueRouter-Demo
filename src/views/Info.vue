d38a3ed04f1667de07b704b711c60fee11e7e4ba
YinAnt <Yin_Ant@163.com>
2019-06-27 09:43:18 +0800

    https://github.com/YinAnt/VueRouter-Demo.git

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="info">
     <h1>This is an info page.</h1>
+    <button type="button" @click="add()">add</button>
   </div>
 </template>
+
+<script>
+import store from '@/store'
+export default {
+  name:'Info',
+  methods:{
+    add(){
+      console.log('click add button')
+      store.commit('increace')  // 提交vuex事件
+    }
+  }
+}
+</script>
